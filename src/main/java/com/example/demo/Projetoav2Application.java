91c86d2880661f00c3ea0fc2262f696fc991f1ed
Felipe Morales <fpontesmorales@gmail.com>
2020-05-26 16:30:12 -0300

    Criado operação de instanciação e testando o rest endpoint clientes/id

--- src/main/java/com/example/demo/Projetoav2Application.java
+++ src/main/java/com/example/demo/Projetoav2Application.java
@@ -1,14 +1,32 @@
 package com.example.demo;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Projetoav2Application {
+import com.example.demo.domain.Cliente;
+import com.example.demo.repositories.ClienteRepository;
 
+@SpringBootApplication
+public class Projetoav2Application implements CommandLineRunner {
+	
+	@Autowired
+	ClienteRepository clienterepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Projetoav2Application.class, args);
-		System.out.println("Testando ambiente SpringBoot");
-	}
 
+	}
+	
+	@Override
+	public void run(String...args)throws Exception {
+		
+		Cliente c1 = new Cliente(null, "Jucileudo Arantes", "Rua 13 de Maio, Bela Vista, 51", "3482-5677", "juju@gmail.com");
+		Cliente c2 = new Cliente(null, "Carmelita Donca", "Av General Sampaio, Centro, 101", "3252-1258", "kaka@gmail.com");
+		
+		this.clienterepository.saveAll(Arrays.asList(c1,c2));
+	}
 }
--- src/main/java/com/example/demo/resources/ClienteResources.java
+++ src/main/java/com/example/demo/resources/ClienteResources.java
@@ -22,7 +22,7 @@ public class ClienteResources {
 	ClienteService service;
 	
 	
-	@RequestMapping(value = "{/id}" , method = RequestMethod.GET)
+	@RequestMapping(value = "/{id}" , method = RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id ){
 		
 		Cliente obj = service.buscar(id);
