db3e4543177cc7f47491050823f42705e8815cbd
Felipe Morales <fpontesmorales@gmail.com>
2020-05-26 09:53:07 -0300

    Primeiro commit para testar o funcionamento do ambiente e da aplicação, e também testar os commits realizados através do Cliente GIT PC

---
+++ src/main/java/com/example/demo/Projetoav2Application.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projetoav2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projetoav2Application.class, args);
+		System.out.println("Testando ambiente SpringBoot");
+	}
+
+}
