16fa24c97ffa6e1b28cb4e6d9c0419724f084790
Felipe Morales <fpontesmorales@gmail.com>
2020-05-29 10:32:53 -0300

    Adicionada a classe Espécie

--- src/main/java/com/example/demo/Projetoav2Application.java
+++ src/main/java/com/example/demo/Projetoav2Application.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.example.demo.domain.Cliente;
+import com.example.demo.domain.Especie;
 import com.example.demo.repositories.ClienteRepository;
+import com.example.demo.repositories.EspecieRepository;
 
 @SpringBootApplication
 public class Projetoav2Application implements CommandLineRunner {
 	
 	@Autowired
 	ClienteRepository clienterepository;
+	
+	@Autowired
+	EspecieRepository especierepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Projetoav2Application.class, args);
@@ -24,9 +29,16 @@ public class Projetoav2Application implements CommandLineRunner {
 	@Override
 	public void run(String...args)throws Exception {
 		
+		Especie esp1 = new Especie (null, "Felino");
+		Especie esp2 = new Especie (null, "Canis");	
+		this.especierepository.saveAll(Arrays.asList(esp1,esp2));
+
+		
 		Cliente c1 = new Cliente(null, "Jucileudo Arantes", "Rua 13 de Maio, Bela Vista, 51", "3482-5677", "juju@gmail.com");
 		Cliente c2 = new Cliente(null, "Carmelita Donca", "Av General Sampaio, Centro, 101", "3252-1258", "kaka@gmail.com");
-		
 		this.clienterepository.saveAll(Arrays.asList(c1,c2));
+		
+			
+		
 	}
 }
